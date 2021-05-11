ae79d6146331d922fa7e947d2fd65835076bbe83
vignesh <vigneshmarimuthu2302@gmail.com>
2021-05-11 10:48:19 +0530

    optimised UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,6 +11,15 @@ class _HomeScreenState extends State<HomeScreen> {
   Dialogflow dialogflow;
   AuthGoogle authGoogle;
   List<ChatMessage> messages = [];
+  List<ChatMessage> mockMessages = [
+    ChatMessage(messageContent: "hai", messageType: "sender"),
+    ChatMessage(messageContent: "Hello", messageType: "receiver"),
+    ChatMessage(messageContent: "how are you", messageType: "sender"),
+    ChatMessage(messageContent: "I am fine. wbu?", messageType: "receiver"),
+    ChatMessage(messageContent: "I am good too", messageType: "sender"),
+    ChatMessage(
+        messageContent: "That is good to hear", messageType: "receiver"),
+  ];
   TextEditingController _inputMessageController = new TextEditingController();
 
   ScrollController _scrollController = new ScrollController(
@@ -21,7 +30,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration(milliseconds: 300), () async {
+    Future.delayed(Duration.zero, () async {
       await initiateDialogFlow();
     });
   }
@@ -36,6 +45,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    _scrollToBottom();
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -47,16 +57,11 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Row(
               children: <Widget>[
                 IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
+                  onPressed: () {},
                   icon: Icon(
                     Icons.arrow_back,
                     color: Colors.black,
                   ),
-                ),
-                SizedBox(
-                  width: 2,
                 ),
                 SizedBox(
                   width: 12,
@@ -67,7 +72,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       Text(
-                        "Kriss Benwat",
+                        "Vignesh Marimuthu",
                         style: TextStyle(
                             fontSize: 16, fontWeight: FontWeight.w600),
                       ),
@@ -81,110 +86,111 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     ],
                   ),
-                ),
-                Icon(
-                  Icons.settings,
-                  color: Colors.black54,
-                ),
+                )
               ],
             ),
           ),
         ),
       ),
-      body: Center(
-          child: Container(
-        child: Align(
-          alignment: Alignment.bottomCenter,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Flexible(
-                child: SingleChildScrollView(
-                  controller: _scrollController,
-                  physics: BouncingScrollPhysics(),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      ListView.builder(
-                          itemCount: messages.length,
-                          shrinkWrap: true,
-                          padding: EdgeInsets.only(top: 10, bottom: 10),
-                          physics: NeverScrollableScrollPhysics(),
-                          itemBuilder: (context, index) {
-                            return Container(
-                              padding: EdgeInsets.only(
-                                  left: 14, right: 14, top: 10, bottom: 10),
-                              child: Align(
-                                alignment:
-                                    (messages[index].messageType == "receiver"
-                                        ? Alignment.topLeft
-                                        : Alignment.topRight),
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20),
-                                    color: (messages[index].messageType ==
-                                            "receiver"
-                                        ? Colors.grey.shade200
-                                        : Colors.blue[200]),
-                                  ),
-                                  padding: EdgeInsets.all(16),
-                                  child: Text(
-                                    messages[index].messageContent,
-                                    style: TextStyle(fontSize: 15),
-                                  ),
-                                ),
-                              ),
-                            );
-                          }),
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(left: 10, bottom: 10, top: 10),
-                height: 60,
-                width: double.infinity,
-                child: bottomChatView(),
-              )
-            ],
-          ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            chatSpaceWidget(),
+            Container(
+              height: 1.0,
+              width: double.infinity,
+              color: Colors.blueGrey,
+            ),
+            bottomChatView()
+          ],
         ),
-      )),
+      ),
+    );
+  }
+
+  Widget chatSpaceWidget() {
+    return Flexible(
+      child: SingleChildScrollView(
+        controller: _scrollController,
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            ListView.builder(
+                itemCount: mockMessages.length,
+                shrinkWrap: true,
+                padding: EdgeInsets.only(top: 10, bottom: 10),
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: EdgeInsets.only(
+                        left: 14, right: 14, top: 10, bottom: 10),
+                    child: Align(
+                      alignment: (mockMessages[index].messageType == "receiver"
+                          ? Alignment.topLeft
+                          : Alignment.topRight),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: (mockMessages[index].messageType == "receiver"
+                              ? Colors.grey.shade200
+                              : Colors.blue[200]),
+                        ),
+                        padding: EdgeInsets.all(16),
+                        child: Text(
+                          mockMessages[index].messageContent,
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+          ],
+        ),
+      ),
     );
   }
 
   Widget bottomChatView() {
-    return Row(
-      children: <Widget>[
-        SizedBox(
-          width: 15,
-        ),
-        Expanded(
-          child: TextField(
-            controller: _inputMessageController,
-            // onSubmitted: fetchFromDialogFlow(_inputMessageController.text),
-            decoration: InputDecoration(
-                hintText: "Write message...",
-                hintStyle: TextStyle(color: Colors.black54),
-                border: InputBorder.none),
+    return Container(
+      padding: EdgeInsets.only(left: 10, bottom: 10, top: 10),
+      height: 60,
+      width: double.infinity,
+      child: Row(
+        children: <Widget>[
+          SizedBox(
+            width: 15,
           ),
-        ),
-        SizedBox(
-          width: 15,
-        ),
-        FloatingActionButton(
-          onPressed: () {
-            fetchFromDialogFlow(_inputMessageController.text);
-          },
-          child: Icon(
-            Icons.send,
-            color: Colors.white,
-            size: 18,
+          Expanded(
+            child: TextField(
+              controller: _inputMessageController,
+              onSubmitted: (String str) {
+                fetchFromDialogFlow(str);
+              },
+              decoration: InputDecoration(
+                  hintText: "Write message...",
+                  hintStyle: TextStyle(color: Colors.black54),
+                  border: InputBorder.none),
+            ),
           ),
-          backgroundColor: Colors.blue,
-          elevation: 0,
-        ),
-      ],
+          SizedBox(
+            width: 15,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              fetchFromDialogFlow(_inputMessageController.text);
+            },
+            child: Icon(
+              Icons.send,
+              color: Colors.white,
+              size: 18,
+            ),
+            backgroundColor: Colors.blue,
+            elevation: 0,
+          ),
+        ],
+      ),
     );
   }
 
